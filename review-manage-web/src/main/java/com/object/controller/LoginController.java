22ab01d95c3b0459440846eb0c9e551fdaf24798
t123321 <2231654262@qq.com>
2019-01-15 08:32:08 +0800

    新增了后台传json格式到前台的配置类 在spring-xml里面

--- review-manage-web/src/main/java/com/object/controller/LoginController.java
+++ review-manage-web/src/main/java/com/object/controller/LoginController.java
@@ -4,35 +4,32 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.object.pojo.Admin;
 import com.object.service.AdminServiceImpl;
 
 @Controller
-@RequestMapping("/AdminController")
+@RequestMapping("/LoginController")
 /**
  * 登录页面控制器(Controller)
- * @author Administrator
+ * @author LoginController
  *
  */
 public class LoginController {
+		@Autowired
 	 	private AdminServiceImpl adminService;
 	    
-	    public AdminServiceImpl getAdminService() {
-	        return adminService;
-	    }
-
-	    @Autowired
-	    public void setAdminService(AdminServiceImpl adminService) {
-	        this.adminService = adminService;
-	    }
-
-	    @RequestMapping("/showAdmin")
-	    public String showPersons(Model model){
-	        List<Admin> admin = adminService.loadAdmin();
-	        model.addAttribute("admins", admin);
+	    @RequestMapping()
+	    public String showLogin(){
 	        return "login";
+	    }
+
+	    @ResponseBody
+	    @RequestMapping("/showAdmin")
+	    public List<Admin> showAdmin(){
+	        List<Admin> admin = adminService.loadAdmin();
+	        return admin;
 	    }
 }
